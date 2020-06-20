31f4a51a604396ad23806190cd9c53c1deec0728
yumikan <yumikan@amazon.co.jp>
2020-06-20 09:26:04 +0000

    Introducing new code that is potentially thread unsafe and inefficient
    Signed-off-by: yumikan <yumikan@amazon.co.jp>

--- src/main/java/com/company/sample/application/CreateOrderThread.java
+++ src/main/java/com/company/sample/application/CreateOrderThread.java
@@ -61,6 +61,10 @@ public class CreateOrderThread extends Thread{
 			}
 
 			SalesSystem.orders.put(orderDate, order);
+			//Check if the Order entered and present
+            if (SalesSystem.orders.containsKey(orderDate)) {
+                    System.out.println("New order verified to be present in hashmap: " + SalesSystem.orders.get(orderDate)); 
+            }
 			id++;
 		} catch (IllegalArgumentException e){
             //e.printStackTrace();
